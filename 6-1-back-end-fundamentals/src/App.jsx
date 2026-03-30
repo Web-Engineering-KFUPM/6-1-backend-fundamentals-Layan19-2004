19fb1042259ad69026a9d554b1ee7d10976f508f
Layan-2004 <layanalasmari25@gmail.com>
2026-03-30 13:10:11 +0300

    todo8

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -370,6 +370,12 @@ export default function App() {
   //     setStudent(data);
   //   });
 
+  fetch('http://localhost:3000/student')
+      .then((res) => res.json())
+      .then((data) => {
+            setStudent(data);
+      });
+
   return (
     <main className="app-shell">
       <section className="hero-box">
